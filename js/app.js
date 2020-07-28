9616c097c3ae7b5e5af2e6f971c314cbdbfccab6
Mariano Cocuche <mariano.cocuche@gmail.com>
2020-07-28 19:10:07 -0300

    push

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 "use strict"
 
-//********************************
-//*** Eventos del mouse
+var N=3, 
+    consultas= [1, 2, 3, 2, 4, 2, 3, 3, 4, 2, 1, 2],
+    consulta = consultas.length,
+    cache = [0, 0, 0],
+    segundos = 0,
+    ValorMaximo=0,
+    i=0;
 
+for(i=0; i<consulta; i++){
+   
+   if(segundos>ValorMaximo) ValorMaximo=segundos;
+   if(cache[0]==consultas[i] || cache[1]==consultas[i] || cache[2]==consultas[i])
+   segundos++;
+   else{
+               cache[0] = cache[1];
+               cache[1] = cache[2];
+               cache[2] = consultas[i];
+               while(cache[0]==0 || cache[1]==0 || cache[2]==0) segundos++;
+   }
 
-const boton = document.querySelector('.boton');
-
-boton.addEventListener('click', function () {
-    console.log("El boton se ha pulsado");
-})
-
-boton.addEventListener('mouseover', function () {
-    console.log("El mouse esta sobre el botón");
-})
-
-boton.addEventListener('mouseout', function () {
-    console.log("El mouse esta fuera del botón");
-})
+}
